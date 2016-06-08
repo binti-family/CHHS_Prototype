3e0dae5267c51ed38b2d383b96ca97c44847e324
Felicia Curcuru <felicia.curcuru@gmail.com>
2016-06-08 01:21:47 -0400

    edited map copy and tests

--- spec/features/i18n_spec.rb
+++ spec/features/i18n_spec.rb
@@ -5,21 +5,21 @@ feature "Viewing site en Español" do
     visit root_path
 
     expect(page.current_path).to eq("/")
-    expect(page).to have_content("Locate foster care service provider near you")
+    expect(page).to have_content("Locate foster care service providers near you")
     expect(page).to have_no_content("Localiza proveedor de servicios de"\
       " cuidado temporal cerca de usted")
 
     click_link "Español"
 
     expect(page.current_path).to match("/es")
-    expect(page).to have_no_content("Locate foster care service provider near you")
+    expect(page).to have_no_content("Locate foster care service providers near you")
     expect(page).to have_content("Localiza proveedor de servicios de"\
       " cuidado temporal cerca de usted")
 
     click_link "English"
 
     expect(page.current_path).to eq("/")
-    expect(page).to have_content("Locate foster care service provider near you")
+    expect(page).to have_content("Locate foster care service providers near you")
     expect(page).to have_no_content("Localiza proveedor de servicios de"\
       " cuidado temporal cerca de usted")
   end
